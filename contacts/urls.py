3ea41b01e073541cbec783da5dfcfceca47e5274
Mohamed Yahya Dkhila <23063@supnum.mr>
2025-03-25 16:07:22 +0000

    second commit : urlnames

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .views import contactList ,contactDetail ,contactCreate,contactUpdate ,contactDelete
+app_name='contacts'
 urlpatterns=[
-    path('',contactList),
-    path('create/',contactCreate),
-    path('<int:pk>/',contactDetail),
-    path('<int:pk>/update/',contactUpdate),
-    path('<int:pk>/delete/',contactDelete),
+    path('',contactList,name='contact-list'),
+    path('create/',contactCreate ,name='contact-create'),
+    path('<int:pk>/',contactDetail,name='contact-detail'),
+    path('<int:pk>/update/',contactUpdate ,name='contact-update'),
+    path('<int:pk>/delete/',contactDelete ,name='contact-delete'),
 ]
